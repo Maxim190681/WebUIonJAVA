561032b3e9832beabbaa452dd05cc46393791773
Zhegunov_Maxim <zhegunov.m@gmail.com>
2022-04-27 23:47:54 +0300

    Less3Investigation

---
+++ lesson3/SeleniumStart.java
@@ -0,0 +1,24 @@
+package org.example.lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager; //ставить обязательно, тесты на самой новой версии
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumStart {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+
+        // библитека BoniGarcia
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver fireFoxDriver = new FirefoxDriver();
+        fireFoxDriver.get("https://ya.ru/");
+
+        Thread.sleep(5000);
+        fireFoxDriver.quit();
+        driver.quit();
+    }
+}
